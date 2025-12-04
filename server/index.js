a47e9bd1a06665f3a8b75a4c4b637fcf46c1585c
suronjit02 <suronjit02@gmail.com>
2025-12-04 23:09:34 +0600

    create database and api

--- server/index.js
+++ server/index.js
@@ -1,1 +1,55 @@
-console.log("server");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = 3000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://TravelEase:LvP42ffUdguMJ26t@cluster0.gotmti8.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    
+      await client.connect();
+      
+      const database = client.db('travelEaseData');
+      const vehicles = database.collection('vehicles');
+
+    app.post("/vehicles", async (req, res) => {
+      const data = req.body;
+        console.log(data);
+        const result = await vehicles.insertOne(data);
+        res.send(result);
+    });
+ 
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("hello developers");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
