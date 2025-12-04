e8b2c14e262cd03328dc6fec68b82cf0ce16cd70
suronjit02 <suronjit02@gmail.com>
2025-12-05 03:47:00 +0600

    change database name and get api create

--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 const uri =
   "mongodb+srv://TravelEase:LvP42ffUdguMJ26t@cluster0.gotmti8.mongodb.net/?appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -21,26 +20,29 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
-      await client.connect();
-      
-      const database = client.db('travelEaseData');
-      const vehicles = database.collection('vehicles');
+    await client.connect();
+
+    const database = client.db("travelEaseData");
+    const vehicles = database.collection("vehicles");
 
     app.post("/vehicles", async (req, res) => {
       const data = req.body;
-        console.log(data);
-        const result = await vehicles.insertOne(data);
-        res.send(result);
+      console.log(data);
+      const result = await vehicles.insertOne(data);
+      res.send(result);
     });
- 
-    
+
+    // get api
+    app.get("/vehicles", async (req, res) => {
+      const result = await vehicles.find().toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    
     //   await client.close();
   }
 }
