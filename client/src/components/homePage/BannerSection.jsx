70b18adcd67c9ec33dcbb48e7896ee8eba505839
suronjit02 <suronjit02@gmail.com>
2026-01-03 22:44:22 +0600

    hero section update

--- client/src/components/homePage/BannerSection.jsx
+++ client/src/components/homePage/BannerSection.jsx
@@ -4,22 +4,22 @@ import { Link } from "react-router";
 const BannerSection = () => {
   return (
     <div
-      className="relative h-[90vh] bg-cover bg-center"
+      className="relative h-[80vh] bg-cover bg-center"
       style={{ backgroundImage: "url('/vehicle.png')" }}
     >
-      <div className="bg-black/50 w-full h-full flex flex-col justify-evenly items-center text-center text-white p-4">
+      <div className="bg-black/60 w-full h-full flex flex-col justify-evenly items-center text-center text-white p-4">
         <div className="flex flex-col justify-center items-center">
-          <h1 className="text-5xl font-bold  mb-4">
+          <h1 className="text-2xl sm:text-5xl font-bold  mb-4">
             TravelEase – Book Your Journey
           </h1>
-          <p className="max-w-xl mb-6 text-lg text-gray-300">
+          <p className="max-w-xl text-lg text-gray-300">
             Choose your perfect vehicle and plan your trip with ease.
           </p>
         </div>
 
         <Link
           to="/all-vehicles"
-          className="btn border-none bg-linear-to-r bg-sky-600 hover:bg-sky-700 mt-3 inline-block px-4 py-2 text-white"
+          className="btn border-none primary text-white "
         >
           Explore All Vehicles
         </Link>
